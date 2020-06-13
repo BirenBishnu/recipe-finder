a2ffb7924c6b57d8153c1afe918e907283d4f21f
Biren Bishnu <birenbishnu@gmail.com>
2020-06-14 04:08:40 +0530

    Base Code Cleanup

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Heading.css';
+
+
+class Heading extends Component {
+  render() {
+    return (
+      <div >
+      
+      </div>
+    );
+  }
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Heading from './Heading';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <Heading />
   </React.StrictMode>,
   document.getElementById('root')
 );
