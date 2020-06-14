be04715b776d8c83be2198d9cc19aed87ef9cfd3
Biren Bishnu <birenbishnu@gmail.com>
2020-06-14 07:15:22 +0530

    Added Title and Input box and search button

--- src/Heading.js
+++ src/Heading.js
@@ -5,8 +5,17 @@ import './Heading.css';
 class Heading extends Component {
   render() {
     return (
-      <div >
-      
+      <div className="container">
+        <p style={{ fontSize: '200%', color: 'grey' }}>Recipe Finder</p>
+        <div>
+          <span>
+            <input type="text" name="dishName" onChange={(event) => this.handleChange(event)} placeholder="Enter the Name of the Dish" />
+          </span>
+          <span>
+            <input type="button" value="Get Recipes" onClick={() => this.handleGetRecipes()} />
+          </span>
+        </div>
+        <h3>Type a Dish Name to Search for it's ingredients</h3>
       </div>
     );
   }
